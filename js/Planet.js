acde05ea875103e3a0a0fa9aca550d9d661ee00a
remicmacs <do.remimi.fa@gmail.com>
2018-10-20 22:17:01 +0200

    Added some comments and simplified some chunks of code
    In theory, no breaking changes are made by this commit. Some variables
    have been converted to constants as they were never mutated.

--- js/Planet.js
+++ js/Planet.js
@@ -8,18 +8,27 @@ class Planet {
 
 		this.moons = [];
 
+		// Creating 3D geometry
 		this.geometry = new THREE.SphereBufferGeometry(this.radius, 50, 50);
-		let material = new THREE.MeshBasicMaterial(
-			{	transparent: true
-			,	color: '#ffffff'
-			,	map: this.texture
-			});
+		const material = new THREE.MeshBasicMaterial({
+			transparent: true,
+			color: '#ffffff',
+			map: this.texture
+		});
+
+		// Mapping texture
 		this.mesh = new THREE.Mesh(this.geometry, material);
+
+		// Positionning object in scene
 		this.mesh.position.z = -10;
 		this.mesh.position.x = this.distance;
 		this.mesh.rotation.reorder("ZYX");
+
+		// Binding instance to Mesh object (why ?)
 		this.mesh.name = this.name;
-		this.mesh.userData = { object: this };
+		this.mesh.userData = {
+			object: this
+		};
 		this.updateRotation();
 	}
 
@@ -28,20 +37,22 @@ class Planet {
 	}
 
 	updateRotation(portrait) {
-		let rotation = this.tilt +
-			( portrait
-			?	-Math.PI / 2
-			:	0
+		const rotation = this.tilt +
+			(portrait ?
+				-Math.PI / 2 :
+				0
 			);
 
-		let target = new THREE.Euler(0, 0, rotation);
-		let current = this.mesh.rotation.clone();
-		let tween = new TWEEN.Tween(current)
+		const target = new THREE.Euler(0, 0, rotation);
+		const current = this.mesh.rotation.clone();
+
+		// Creating animation
+		const tween = new TWEEN.Tween(current)
 			.to(target, 1000)
 			.easing(TWEEN.Easing.Bounce.Out);
 		tween.onUpdate(() => {
 			this.mesh.rotation.z = current.z;
 		})
-		tween.start();
+		tween.start(); // Launching animation
 	}
 }
