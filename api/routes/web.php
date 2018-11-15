151f9c6f39ba8546c8a210c830761592167d99f4
remicmacs <rembourgeon@gmail.com>
2018-11-15 13:43:48 +0100

    Back-end framework added with routes

---
+++ api/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+use Illuminate\Http\Response;
+
+$router->get('hello/{name}', function($name) {
+    $content = array("data" => "hello $name");
+    $value = "application/json";
+    return response()->json($content);
+});
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
