ef3cefa84c0eed9ec7dc75007f29241de0e16a17
remicmacs <rembourgeon@gmail.com>
2018-11-17 18:50:15 +0100

    Moved code for connection in its own file

--- index.js
+++ index.js
@@ -45,34 +45,7 @@ document.addEventListener('touchstart', onTouchStart);
 document.addEventListener('touchend', onTouchEnd);
 
 // Handler for connection with form
-document.connect.onsubmit = function(event) {
-	event.preventDefault();
-
-	// Recovering form data
-	const data = new FormData(event.target);
-
-	/*
-	// Debug
-	const otherdata = {};
-	for (const pair of new FormData(event.target)) {
-		otherdata[pair[0]] = pair[1];
-	}
-	console.log(otherdata);
-	*/
-
-	// Recovering target URL
-	const url = event.target.action;
-	fetch(url, {
-		method: 'POST',
-		body: data,
-	})
-	// Unpack JSON body
-	.then(res => res.json())
-	// Handle all responses even 403 / 500s etc.
-	.then(response => console.log('Success:', JSON.stringify(response)))
-	// If this fails, it must be a network error
-	.catch(error => console.error("Fatal Error : ", error));
-}
+document.connect.onsubmit = (new ConnectionHandler).connect;
 
 const events = [
 	['userImage', 'mouseup', openMenu],
---
+++ js/ConnectionHandler.js
@@ -0,0 +1,50 @@
+/**
+ * Class to hold all state and methods for login process
+ *
+ * @class ConnectionHandler
+ */
+class ConnectionHandler {
+
+  /**
+   * Event handler for connection form
+   *
+   * Triggered on `submit` event.
+   *
+   * Intercept normal event behaviour and connects the user with AJAX requests
+   *
+   * @param {*} event
+   * @memberof ConnectionHandler
+   */
+  connect(event) {
+    event.preventDefault();
+    // Recovering form data
+    const data = new FormData(event.target);
+
+    /*
+    // Debug
+    const otherdata = {};
+    for (const pair of new FormData(event.target)) {
+      otherdata[pair[0]] = pair[1];
+    }
+    console.log(otherdata);
+    */
+
+    // Recovering target URL
+    const url = event.target.action;
+    fetch(url, {
+      method: 'POST',
+      body: data,
+    })
+    // Unpack JSON body
+    .then(res => res.json())
+    // Handle all responses even 403 / 401 / 500s etc.
+    // If code is not 200 => display information in red modal div
+    // else make connection window disappear, display username in menu,
+    //   and modify menu options to display leaderboards (and account management)
+    .then(response => console.log('Success:', JSON.stringify(response)))
+
+    // If this fails, it must be a network error and I frankly don't know what
+    //   to do...
+    .catch(error => console.error("Fatal Error : ", error));
+  }
+}
