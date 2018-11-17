3702969fa5644dcdf488ff4085c1452c0c82f456
remicmacs <rembourgeon@gmail.com>
2018-11-17 20:17:51 +0100

    Added JWT library in ConnectionController

--- api/app/Http/Controllers/ConnectionController.php
+++ api/app/Http/Controllers/ConnectionController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 // To be able to create cookies to attach to response
 use Symfony\Component\HttpFoundation\Cookie;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
 
 class ConnectionController extends Controller
 {
+    private $jwt_secret;
     /**
      * Create a new controller instance.
      *
@@ -16,7 +19,9 @@ class ConnectionController extends Controller
      */
     public function __construct()
     {
-        // Nothing to do yet
+        // Recover config values
+        $this->jwt_secret = env("JWT_SECRET");
+
         // Maybe DI of Repository object
     }
 
@@ -26,15 +31,24 @@ class ConnectionController extends Controller
      * @return Illuminate\Http\Response
      */
     public function connect() {
-        // Sometimes, $_POST is unset, if not accessed by the proper means
-        $username = filter_input(INPUT_POST, 'username');
-        $password = filter_input(INPUT_POST, 'password');
+        // Sometimes, $_POST is unset, if the connection does not come from
+        // dedicated web app.
+        $username = filter_input(
+            INPUT_POST, 'username',
+            FILTER_SANITIZE_FULL_SPECIAL_CHARS
+        );
+        $password = filter_input(
+            INPUT_POST,
+            'password',
+            FILTER_SANITIZE_FULL_SPECIAL_CHARS
+        );
 
         // If any of the vars is null, it means it was not set in $_POST
         // superglobal variable upon receiving request.
         if ($username === null || $password === null
             || $username !== "remicmacs" || $password !== "prout"
         ) {
+            // @TODO
             // User credentials verification should take place here
 
             // Add a message to display in modal window
@@ -42,12 +56,42 @@ class ConnectionController extends Controller
                 ->header("Content-type", "application/json");
         }
 
+        // Must instantiate signer for signature
+        $signer = new Sha256();
         // Manage JWT (Javascript Web Token) creation
-        $content = array("token" => "hello");
+        $issuer_url = env("APP_URL");
+        // Trying to make jti as unique as possible
+        $jti_claim = hash("sha256", $username.$password.(time()));
+        $token = (new Builder())
+            // iss claim = issuer of the token
+            ->setIssuer($issuer_url)
+            // aud claim = target audience for the token
+            ->setAudience($issuer_url)
+            // jti claim = unique id that will not have hash collision with
+            //    another generated JWT
+            ->setId($jti_claim, true)
+            // iat claim = timestamp when the JWT has been generated
+            ->setIssuedAt(time())
+            // nbf claim = timestamp of the beginning of the JWT validity period
+            ->setNotBefore(time() + 1)
+            // exp claim = timestamp for expiration time
+            ->setExpiration(time() + 3600)
+            // Set uid to avoid to recover uid every time in database
+            ->set('uid', 1)
+            // Signing the JWT
+            ->sign($signer, $this->jwt_secret)
+            // Retrieves the generated token
+            ->getToken();
+
+        // Forcing token to be in string value
+        // __tostring() magin method of Token encodes in base64 and concatenates
+        // every three part of JWT (headers.claims.signature)
+        $token = (string) $token;
+
+        // Attaching JWT to response
+        $content = array("JWT" => $token);
         $value = "application/json";
         return response($content)
-            ->header("Content-type", "application/json")
-            // Is the cookie necessary ?
-            ->cookie(new Cookie('token', 'jesuisconnectesisijevousjure', 60));
+            ->header("Content-type", "application/json");
     }
 }
