37c6a0d055ed3a5fb42a9e093e528f314a9d3683
remicmacs <rembourgeon@gmail.com>
2018-12-01 08:59:59 +0100

    Added script for mock values

---
+++ database_scripts/tags_testdata.sql
@@ -0,0 +1,17 @@
+insert into labels (name) values
+("Naine blanche"),
+("Géante rouge"),
+("Nébuleuse"),
+("Satellite"),
+("555 kHz");
+
+insert into tags (celestial_bodies_id, label_id, userid) values
+(1, 1, 1),
+(1, 2, 1),
+(1, 3, 1),
+(1, 4, 1),
+(1, 5, 1),
+(2, 1, 1),
+(2, 3, 1),
+(4, 1, 1),
+(4, 5, 1);
